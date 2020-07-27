0718de6f8941e39c9d7d28d454413148f3a36921
Benjamin Sklar <nyjetsny1@gmail.com>
2020-07-27 09:45:16 -0700

    Add files via upload

---
+++ jstest2.js
@@ -0,0 +1,6 @@
+(function(w, d, s, f) {
+w[f] = w[f] || {conf: function () { (w[f].data = w[f].data || []).push(arguments);}};
+var l = d.createElement(s), e = d.getElementsByTagName(s)[0];
+l.async = 1; l.src = 'https://ws.facil-iti.com/tag/faciliti-tag.min.js'; e.parentNode.insertBefore(l, e);
+}(window, document, 'script', 'FACIL_ITI'));
+FACIL_ITI.conf('userId', '043ba29c-36e8-11ea-8091-0242ac130002');
